e856650036d6c258453572a508d8b1fa60d8cb47
metherton <martinetherton63@gmail.com>
2024-06-12 22:55:46 +0200

    exercise

--- src/main/scala/part2AbstractMath/Monoids.scala
+++ src/main/scala/part2AbstractMath/Monoids.scala
@@ -1,7 +1,51 @@
 package part2AbstractMath
 
 object Monoids {
-  def main(args: Array[String]): Unit = {
 
+  import cats.Semigroup
+  import cats.instances.int._
+  import cats.syntax.semigroup._ // so that we can import the |+| extension method
+
+  val numbers = (1 to 1000).toList
+  // |+| is always associative
+
+  val sumLeft = numbers.foldLeft(0)(_ |+| _)
+  val sumRight = numbers.foldRight(0)(_ |+| _)
+
+  // define a general API
+//  def combineFold[T](list: List[T])(implicit semigroup: Semigroup[T]): T = {
+//    list.foldLeft(/* WHAT ? */)(_ |+| _)
+//  }
+
+  // MONOIDS
+  import cats.Monoid
+  val intMonoid = Monoid[Int]
+  val combineInt = intMonoid.combine(23, 999)
+  val zero = intMonoid.empty // 0
+
+  import cats.instances.string._ // bring the implicit Monoid[String] in scope
+  val emptyString = Monoid[String].empty // ""
+  val combineString = Monoid[String].combine("I understand ", "monoids")
+
+  import cats.instances.option._ // construct an implicit Monoid[Option[Int]]
+  val emptyOption = Monoid[Option[Int]].empty // None
+  val combineOption = Monoid[Option[Int]].combine(Option(2), Option.empty[Int]) // Some(2)
+
+  // extension method for Monoids - |+|
+  //import cats.syntax.monoid._ // either this one or cat.syntax.semigroup
+  val combinedOptionFancy = Option(3) |+| Option(7)
+
+  // TODO: 1 implement a combineFold
+  def combineFold[T](list: List[T])(implicit monoid: Monoid[T]): T = {
+    list.foldLeft(monoid.empty)(_ |+| _)
+  }
+
+
+
+
+  def main(args: Array[String]): Unit = {
+    println(sumLeft)
+    println(sumRight)
+    println(combineFold(numbers))
   }
 }
