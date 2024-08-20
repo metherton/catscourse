f9bd1a24b0a3018731ff69cc8f6ede666c887af1
metherton <martinetherton63@gmail.com>
2024-08-20 14:51:08 +0200

    effects

--- src/main/scala/part2Effects/Effects.scala
+++ src/main/scala/part2Effects/Effects.scala
@@ -1,6 +1,8 @@
 package part2Effects
 
+import java.time.InstantSource.system
 import scala.concurrent.Future
+import scala.io.StdIn
 
 object Effects {
 
@@ -77,7 +79,87 @@ object Effects {
     42
   })
 
+  /*
+
+      Exercises
+      1. An IO (use MyIO) which returns the current time of the system
+      2. An IO which measures the duration of a computation (i.e an IO unsafeRun) (hint: use ex 1)
+      3. An IO which prints something to the console
+      4. An IO which reads something from the console,i.e stdin
+
+
+   */
+
+  // 1
+  def clock: MyIO[Long] = MyIO(() => system.millis())
+
+  // 2
+  def measure[A](computation: MyIO[A]): MyIO[Long] = for {
+    startTime <- clock
+    _ <- computation
+    finishTime <- clock
+  } yield finishTime - startTime
+
+  /*
+      clock.flatMap(startTime => computation.flatMap(_ => clock.map(finishTime => finishTime - startTime)))
+
+      clock.map(finishTime => finishTime - startTime) = MYIO(() => clock.unsafeRun() - startTime)
+                              finishTime - startTime) = MYIO(() => system.millis - startTime)
+
+      => clock.flatMap(startTime => computation.flatMap(_ => MYIO(() => system.millis - startTime)))
+
+      computation.flatMap(lambda) = MyIO(() => lambda(computation.unsafeRun())
+
+      computation.flatMap(lambda) = MyIO(() => lambda(___COMP___).unsafeRun())
+                                  = MyIO(() => MyIO(() => system.millis - startTime)).unsafeRun())
+                                  = MyIO(() => system.millis_after_computation - startTime)
+
+
+      => clock.flatMap(startTime => MyIO(() => system.millis_after_computation - startTime))
+      MyIO(() => lamda(clock.unsafeRun()).unsafeRun() // lamda is startTime => MyIO(() => system.millis_after_computation - startTime)
+      MyIO(() => lamda(clock.unsafeRun()).unsafeRun()
+      MyIO(() => lamda(system.millis).unsafeRun()
+      MyIO(() => MyIO(() => system.millis_after_computation - startTime).unsafeRun()
+      MyIO(() => MyIO(() => system.millis_after_computation - system.millis()).unsafeRun()
+
+      MyIO(() => system.millis_after_computation - system.millis_at_start()
+
+   */
+
+
+  def testTimeIO(): Unit = {
+    val test = measure(MyIO(() => Thread.sleep(1000)))
+    println(test.unsafeRun())
+  }
+
+  // 3
+
+  def putStrLn(line: String): MyIO[Unit] =
+    MyIO(() => println(line))
+
+  // 4
+  val read: MyIO[String] = MyIO(() => StdIn.readLine())
+
+
+
+  def testConsole(): Unit = {
+    val program: MyIO[Unit] = for {
+      line1 <- read
+      line2 <- read
+      _ <- putStrLn(line1 + line2)
+    } yield ()
+    program.unsafeRun()
+  }
+
+  /*
+      line1 = read from console
+      line2 = read from console
+      println( line1 + line 2 )
+
+   */
+
   def main(args: Array[String]): Unit = {
-    anIO.unsafeRun()
+    //testTimeIO()
+    testConsole()
   }
 }
