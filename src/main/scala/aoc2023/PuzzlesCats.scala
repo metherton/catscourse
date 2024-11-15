fe8a418f85116c75fb1fd4c90130e9460cb1ac58
metherton <martinetherton63@gmail.com>
2024-11-15 18:41:20 +0100

    added cats puzzles

--- src/main/scala/aoc2023/PuzzlesCats.scala
+++ src/main/scala/aoc2023/PuzzlesCats.scala
@@ -9,30 +9,27 @@ import scala.concurrent.duration._
 
 object PuzzlesCats extends IOApp.Simple {
 
-  import cats.syntax.functor._
-  import cats.syntax.flatMap._
-  var sum: Int = 0
-  def readLineByLine(scanner: Scanner): IO[Unit] = {
+  def readLineByLine(scanner: Scanner, acc: Int): IO[Unit] = {
     def findNumber(s: String): String = {
       s.find(i => i.isDigit).head.toString + s.findLast(i => i.isDigit).head.toString
     }
     if (scanner.hasNextLine) {
-      //IO(scanner.nextLine()).debug1 >> IO.sleep(100.millis) *> readLineByLine(scanner)
-      val res = (for {
+      for {
         l <- IO(scanner.nextLine()).debug1
-        _ <- IO.sleep(100.millis)
-        r = findNumber(l)
-        _ <- readLineByLine(scanner)
-      } yield ())
-      res
+        _ <- IO.sleep(1.millis)
+        r = findNumber(l).toInt
+        _ <- readLineByLine(scanner, acc + r)
+      } yield ()
     }
-    else IO.unit
+    else {
+      IO(s"final total is $acc").debug1 *> IO.unit
+    }
   }
 
   def bracketReadFile(path: String): IO[Unit] =
     IO(s"opening file at $path").debug1 *>
       openFileScanner(path).bracket { scanner =>
-        readLineByLine(scanner)
+        readLineByLine(scanner, 0)
       } { scanner =>
         IO(s"closing file at $path").debug1 *> IO(scanner.close())
       }
