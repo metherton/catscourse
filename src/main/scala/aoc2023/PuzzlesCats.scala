e41508753954b98fa8173d3572efea782fbdd416
metherton <martinetherton63@gmail.com>
2024-11-15 17:56:47 +0100

    added cats puzzles

--- src/main/scala/aoc2023/PuzzlesCats.scala
+++ src/main/scala/aoc2023/PuzzlesCats.scala
@@ -9,9 +9,25 @@ import scala.concurrent.duration._
 
 object PuzzlesCats extends IOApp.Simple {
 
-  def readLineByLine(scanner: Scanner): IO[Unit] =
-    if (scanner.hasNextLine) IO(scanner.nextLine()).debug1 >> IO.sleep(100.millis) *> readLineByLine(scanner)
+  import cats.syntax.functor._
+  import cats.syntax.flatMap._
+  var sum: Int = 0
+  def readLineByLine(scanner: Scanner): IO[Unit] = {
+    def findNumber(s: String): String = {
+      s.find(i => i.isDigit).head.toString + s.findLast(i => i.isDigit).head.toString
+    }
+    if (scanner.hasNextLine) {
+      //IO(scanner.nextLine()).debug1 >> IO.sleep(100.millis) *> readLineByLine(scanner)
+      val res = (for {
+        l <- IO(scanner.nextLine()).debug1
+        _ <- IO.sleep(100.millis)
+        r = findNumber(l)
+        _ <- readLineByLine(scanner)
+      } yield ())
+      res
+    }
     else IO.unit
+  }
 
   def bracketReadFile(path: String): IO[Unit] =
     IO(s"opening file at $path").debug1 *>
@@ -21,5 +37,5 @@ object PuzzlesCats extends IOApp.Simple {
         IO(s"closing file at $path").debug1 *> IO(scanner.close())
       }
 
-  override def run: IO[Unit] = bracketReadFile("src/main/resources/5.txt").void
+  override def run: IO[Unit] = bracketReadFile("src/main/resources/1.txt").void
 }
