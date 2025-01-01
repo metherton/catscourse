a50d5c66d5b9084f40ee2846376499f3e0cbf047
metherton <martinetherton63@gmail.com>
2025-01-01 13:45:33 +0100

    day 9 part 1 done

--- src/main/scala/aoc2024/Day9.scala
+++ src/main/scala/aoc2024/Day9.scala
@@ -10,28 +10,28 @@ import scala.concurrent.duration._
 
 object Day9  extends IOApp.Simple {
 
-  case class State(fileMap: List[Char]) {
-    def compress(): List[Char] = {
+  case class State(fileMap: List[(Char, Boolean)]) {
+    def compress(): List[(Char, Boolean)] = {
 
-      def findFirstSpace(fmp: List[Char]): Int = {
-        def loop2(fileMap: List[Char], i: Int): Int = fileMap match {
+      def findFirstSpace(fmp: List[(Char,Boolean)]): Int = {
+        def loop2(fileMap: List[(Char,Boolean)], i: Int): Int = fileMap match {
           case Nil => i // if
-          case h :: t if h == '.' => i
+          case h :: t if h._2 => i
           case h :: t => loop2(t, i + 1)
         }
         loop2(fmp, 0)
       }
 
-      def loop(originalReversed: List[Char], originalFileMap: List[Char], count: Int): List[Char] = originalReversed match {
+      def loop(originalReversed: List[(Char, Boolean)], originalFileMap: List[(Char, Boolean)], count: Int): List[(Char, Boolean)] = originalReversed match {
         case Nil => originalFileMap
-        case h :: t if h != '.' => {
+        case h :: t if !h._2 => {
           // we have a file part..we want to find first space in our file map and replace it
           val firstSpacePosition = findFirstSpace(originalFileMap)
           if (firstSpacePosition < originalFileMap.size) {
             val evenNewerFileMap = originalFileMap.updated(firstSpacePosition, h)
             loop(t, evenNewerFileMap, count + 1)
           } else {
-            originalFileMap.take(originalFileMap.size - count) ::: Range(0, count).map(_ => '.').toList
+            originalFileMap.take(originalFileMap.size - count) ::: Range(0, count).map(_ => ('.',true)).toList
           }
 
         }
@@ -41,23 +41,23 @@ object Day9  extends IOApp.Simple {
     }
   }
 
-  def createFileMap(diskMap: List[Char]): List[Char] = {
-    def loop(chars: List[Char], fileNo: Int, acc: List[Char], isSpace: Boolean): List[Char] = chars match {
+  def createFileMap(diskMap: List[Char]): List[(Char, Boolean)] = {
+    def loop(chars: List[Char], fileNo: Int, acc: List[(Char, Boolean)], isSpace: Boolean): List[(Char, Boolean)] = chars match {
       case Nil => acc
-      case h :: t if isSpace => loop(t, fileNo, Range(0, h.asDigit).map(_ => '.').toList ::: acc, !isSpace)
-      case h :: t => loop(t, fileNo + 1, Range(0, h.asDigit).map(_ => fileNo.toChar).toList ::: acc, !isSpace)
+      case h :: t if isSpace => loop(t, fileNo, Range(0, h.asDigit).map(_ => ('.', true)).toList ::: acc, !isSpace)
+      case h :: t => loop(t, fileNo + 1, Range(0, h.asDigit).map(_ => (fileNo.toChar, false)).toList ::: acc, !isSpace)
     //  case h :: t => loop(t, fileNo + 1, if (h.asDigit == 0) println("emptyfilefounnd") else Range(0, h.asDigit).map(_ => fileNo.toChar).toList ::: acc, !isSpace)
 
     }
     loop(diskMap, 0, List(), false)
   }
 
-  def checksum(m: List[Char]): Long = {
-    def loop(chars: List[Char], acc: Long, i: Int): Long = chars match {
+  def checksum(m: List[(Char,Boolean)]): Long = {
+    def loop(chars: List[(Char,Boolean)], acc: Long, i: Int): Long = chars match {
       case Nil => acc
       case h :: t => {
         println(s"$i: $acc")
-        loop(t, if (h != '.') acc + (h.toLong * i) else acc, i + 1)
+        loop(t, if (!h._2) acc + (h._1.toLong * i) else acc, i + 1)
       }
     }
     loop(m, 0, 0)
@@ -70,7 +70,7 @@ object Day9  extends IOApp.Simple {
         _ <- IO(l.toList.zipWithIndex.filter(t => t._2 % 2 == 1).map(_._1.asDigit).sum).debug1
         fileMap = createFileMap(l.toList)
         //fileMap = l.toList.zipWithIndex.flatMap(t => if (t._2 % 2 == 1) Range(0, t._1.asDigit).map(_ => '.') else Range(0, t._1.asDigit).map(_ => (t._2 / 2).toChar)).reverse
-        _ <- IO(fileMap.filter(_ == '.').size).debug1
+        _ <- IO(fileMap.filter(_._2).size).debug1
  //       _ <- IO(fileMap).debug1
 
         _ <- readLineByLine(scanner, state.copy(fileMap = fileMap))
@@ -85,7 +85,7 @@ object Day9  extends IOApp.Simple {
     IO(s"opening file at $path") *>
       getResourceFromFile(path).use {
         scanner =>
-          readLineByLine(scanner, State(List[Char]()))
+          readLineByLine(scanner, State(List[(Char,Boolean)]()))
       }
 
   override def run: IO[Unit] = {
