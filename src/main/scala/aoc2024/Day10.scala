a91509a1d489a8f9f503483f19fdf53068cd191a
metherton <martinetherton63@gmail.com>
2025-01-03 12:50:48 +0100

    day 10 part 1 WIP

--- src/main/scala/aoc2024/Day10.scala
+++ src/main/scala/aoc2024/Day10.scala
@@ -11,12 +11,37 @@ import scala.concurrent.duration._
 object Day10  extends IOApp.Simple {
 
   case class Coords(row: Int, col: Int)
-  case class Trailhead(location: Coords)
   case class Point(id: Int, coords: Coords)
   case class Grid(points: List[Point]) {
+    def left(p: Point): Point = {
+      p
+    }
     def trailheads = points.filter(_.id == 0)
   }
-  case class State(grid: Grid)
+  case class State(grid: Grid) {
+    def paths() = {
+
+      def getNeighbours(p: Point): List[Point] = {
+        val neighbours = grid.left(p) :: Nil
+        neighbours
+      }
+
+      def getPaths(points: List[Point], acc: List[List[Point]]): List[List[Point]] = {
+        for {
+          p <- points
+          neighbours <- List(getNeighbours(p))
+        } yield neighbours
+      }
+
+      def loop(thList: List[Point]):List[List[Point]] = thList match {
+        case Nil => Nil
+        case h :: t => {
+          getPaths(List(h), List()) ::: loop(t)
+        }
+      }
+      loop(grid.trailheads)
+    }
+  }
   def readLineByLine(scanner: Scanner, state: State, rowNo: Int): IO[Unit] = {
     if (scanner.hasNextLine) {
       for {
@@ -28,7 +53,7 @@ object Day10  extends IOApp.Simple {
     }
     else {
       val result = "bla"
-      IO(s"final states is ...$result").debug1 *> IO.unit
+      IO(s"final states is ...${state.paths}").debug1 *> IO.unit
     }
   }
   def resourceReadFile(path: String): IO[Unit] =
